09759600325a95f9ae719184ef6b1a43b6361546
Justin Chiou <chiou.kai@gmail.com>
2023-02-23 15:35:27 -0500

    refactor: moving functions

--- helpers.go
+++ helpers.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"net/http"
 )
 
 // Function getPrefix returns a string based on a conditional sandbox environment
@@ -19,9 +18,9 @@ func (c *Client) getPrefix() string {
 
 // Function baseUrl returns full api url, conditionally checking the prefix based on a possible
 // sandbox environment.
-// func (c *Client) baseUrl() string {
-// 	return fmt.Sprintf("https://%s.norcapsecurities.com/tapiv3/index.php/v3", c.getPrefix())
-// }
+func (c *Client) baseUrl() string {
+	return fmt.Sprintf("https://%s.norcapsecurities.com/tapiv3/index.php/v3", c.getPrefix())
+}
 
 // Function basePostRequest will run a generic http post request.
 func basePostRequest[Request, Response any](c *Client, endpoint string, req *Request) Response {
@@ -34,12 +33,7 @@ func basePostRequest[Request, Response any](c *Client, endpoint string, req *Req
 
 	url := fmt.Sprintf("%s%s", c.baseUrl(), endpoint)
 	byteReader := bytes.NewReader(b)
-	httpRequest, err := http.NewRequest(http.MethodPost, url, byteReader)
-	if err != nil {
-		log.Fatalf("Error forming request for %s: %s", endpoint, err)
-	}
-
-	resp, err := c.HttpClient.Do(httpRequest)
+	resp, err := c.HttpClient.Post(url, "application/json", byteReader)
 	if err != nil {
 		log.Fatalf("Error sending request to %s: %s", endpoint, err)
 	}
