3c2b95fd7189f69321aae3bc1a8700f2e43c15b0
Justin Chiou <chiou.kai@gmail.com>
2023-02-23 17:26:41 -0500

    feat: add unit tests

--- utils/credit_card.go
+++ utils/credit_card.go
@@ -8,16 +8,16 @@ import (
 // Function GetCreditCardType uses regex to determine if the input number is either a VISA,
 // MASTERCARD, or DISCOVER. All other cards return an error, as they are non-compliant with North
 // Capital's transactions.
-func GetCreditCardType(num string) (string, error) {
-	visaRegex := regexp.MustCompile(`/^4\d{3}([-]?)\d{4}\1\d{4}\1\d{4}$/`)
-	mastercardRegex := regexp.MustCompile(`/^5[1-5]\d{2}([-]?)\d{4}\1\d{4}\1\d{4}$/`)
-	discoverRegex := regexp.MustCompile(`/^6(?:011|22(?:1(?=[-]?(?:2[6-9]|[3-9]))|[2-8]|9(?=[-]?(?:[01]|2[0-5])))|4[4-9]\d|5\d\d)([-]?)\d{4}\1\d{4}\1\d{4}$/`)
+func GetCreditCardType(cardNumber string) (string, error) {
+	visaRegex := regexp.MustCompile(`4\d{3}([-]?)\d{4}\d{4}\d{4}`)
+	mastercardRegex := regexp.MustCompile(`5[1-5]\d{2}([-]?)\d{4}\d{4}\d{4}`)
+	discoverRegex := regexp.MustCompile(`6(?:011|22(?:1([-]?(?:2[6-9]|[3-9]))|[2-8]|9([-]?(?:[01]|2[0-5])))|4[4-9]\d|5\d\d)([-]?)\d{4}\d{4}\d{4}`)
 
-	if visaRegex.Find([]byte(num)) != nil {
+	if visaRegex.Find([]byte(cardNumber)) != nil {
 		return "VI", nil
-	} else if mastercardRegex.Find([]byte(num)) != nil {
+	} else if mastercardRegex.Find([]byte(cardNumber)) != nil {
 		return "MC", nil
-	} else if discoverRegex.Find([]byte(num)) != nil {
+	} else if discoverRegex.Find([]byte(cardNumber)) != nil {
 		return "DI", nil
 	}
 	return "", errors.New("Invalid card number.")
---
+++ utils/credit_card_test.go
@@ -0,0 +1,35 @@
+package utils
+
+import (
+	"testing"
+)
+
+// TestGetCreditCardTypeSuccess calls utils.GetCreditCardType with a card number, checking
+// for a valid return value.
+func TestGetCreditCardTypeSuccess(t *testing.T) {
+	numbers := []string{
+		"4111111111111111",
+		"5555555555554444",
+		"6011111111111117",
+	}
+	wanted := []string{
+		"VI",
+		"MC",
+		"DI",
+	}
+	for i := 0; i < len(numbers); i++ {
+		have := numbers[i]
+		want := wanted[i]
+		cardType, err := GetCreditCardType(have)
+		if err != nil {
+			t.Fatalf("GetCreditCardType('%s') error: %s", have, err)
+		}
+		if cardType != want {
+			t.Fatalf("Have: %s, Want: %s", cardType, want)
+		}
+	}
+}
+
+// TestGetCreditCardTypeFailed calls utils.GetCreditCardType with a card number, checking
+// for a valid return value.
+func TestGetCreditCardTypeFailed(t *testing.T) {}
